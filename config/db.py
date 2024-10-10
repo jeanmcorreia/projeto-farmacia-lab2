666040dadfe09fb724e49920c9901f15ebb274e9
Jean Marcos <j.marcoscor04@gmail.comcleargit config --global user.email>
2024-10-10 10:49:19 -0300

    Adicionado .env prevenir exposição de dados sensíveis

--- config/db.py
+++ config/db.py
@@ -1,18 +1,20 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def criar_conexao():
     try:
         conn = psycopg2.connect(
-            dbname='postgres',
-            user='postgres',
-            password='021122',
-            host='127.0.0.1',
-            port='5432'
+            dbname=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT')
         )
         print("Conexão realizada com sucesso!")
         return conn
     except Exception as e:
         print(f"Erro ao conectar com o banco de dados: {e}")
         return None
-    
-criar_conexao()

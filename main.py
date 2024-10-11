bcf52c9932d42b24ca0a942f79423a19effca28f
Kauã Henrique Pugliesi Pontes Santos <kaua.santos@gcl.com.br>
2024-10-11 12:52:29 -0300

    importações das funções para o main e alteração no nome de arquivos (evitar conflitos com o  hifen)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from config.db import criar_conexao
 from login.autenticar import autenticar
 from login.cadastrar import cadastrar
+from pessoas.crud_clientes import criar_cliente, relatorio_cliente, editar_cliente, excluir_cliente
+from pessoas.crud_funcionarios import criar_funcionario, relatorio_funcionario, editar_funcionario, excluir_funcionario
+from produtos.crud_produtos import criar_produtos, relatorio_produtos, editar_produtos, excluir_produtos
+from estoque.crud_estoque import gerar_estoque, relatorio_estoque, editar_estoque, excluir_lote
+
 
 
 user = "admin4"

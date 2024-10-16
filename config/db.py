b8cfcb70a940794988a489d43cba99b6d9779d47
jeanmcorreia <j.marcoscor04@gmail.com>
2024-10-16 09:00:33 -0300

    Baixando a biblioteca python-dotenv e colocando nas depedencias do projeto (requirements.txt)

--- config/db.py
+++ config/db.py
@@ -1,13 +1,18 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 def criar_conexao():
     try:
         conn = psycopg2.connect(
-            dbname='DBNAME',
-            user='DB_USER',
-            password= 'DB_PASSWORD',
-            host= 'DB_HOST',
-            port= 'DB_PORT'
+            dbname=os.getenv('DBNAME'),
+            user=os.getenv('DB_USER'),
+            password= os.getenv('DB_PASSWORD'),
+            host= os.getenv('DB_HOST'),
+            port= os.getenv('DB_PORT')
         )
         print("Conexão realizada com sucesso!")
         return conn

c97d6cb3b51e7117ed4c7e42bce6d843d53b0c2b
Jean Marcos <j.marcoscor04@gmail.com>
2024-11-18 10:04:41 -0300

    Ajustes em repositories (category, client, employee)

--- src/model/repository/category_repository.py
+++ src/model/repository/category_repository.py
@@ -6,43 +6,48 @@ class CategoryRepository:
         self.connection = create_connection()
     
     def report_all_categories(self):
-        query = 'SELECT * FROM \"pharma\".categories;'
+        query = 'SELECT * FROM \"pharma\".categories'
         cursor = self.connection.cursor()
         cursor.execute(query)
         rows = cursor.fetchall()
+        cursor.close()
         
         categories = [Category(*row) for row in rows]
         return categories
     
     def find_category_by_id(self, category_id):
-        query = 'SELECT * FROM \"pharma\".categories WHERE category_id = %s;'
+        query = 'SELECT * FROM \"pharma\".categories WHERE category_id = %s'
         cursor = self.connection.cursor()
         cursor.execute(query, (category_id,))
-        row = cursor.fetchall()
+        row = cursor.fetchone()
+        cursor.close()
 
         return Category(*row) if row else None
     
     def registry_category(self, category):
         query = '''
             INSERT INTO \"pharma\".categories (category_name, description)
-            VALUES(%s, %s);
+            VALUES(%s, %s)
         '''
         cursor = self.connection.cursor()
         cursor.execute(query, (category.category_name, category.description))
         self.connection.commit()
+        cursor.close()
 
     def update_category(self, category):
         query = '''
             UPDATE \"pharma\".categories
             SET category_name = %s, description = %s
-            WHERE category_id = %s;
+            WHERE category_id = %s
         '''
         cursor = self.connection.cursor()
         cursor.execute(query, (category.category_name, category.description, category.category_id))
         self.connection.commit()
+        cursor.close()
 
     def delete_category(self, category_id):
-        query = 'DELETE FROM \"pharma\".categories WHERE category_id = %s;'
+        query = 'DELETE FROM \"pharma\".categories WHERE category_id = %s'
         cursor = self.connection.cursor()
         cursor.execute(query, (category_id,))
         self.connection.commit()
+        cursor.close()
--- src/model/repository/person_employee_repository.py
+++ src/model/repository/person_employee_repository.py
@@ -6,10 +6,11 @@ class PersonEmployeeRepository:
         self.connection = create_connection()
 
     def report_all_employees(self):
-        query = 'SELECT * FROM \"pharma\".employees;'
+        query = 'SELECT * FROM \"pharma\".employees'
         cursor = self.connection.cursor()
         cursor.execute(query)
         rows = cursor.fetchall()
+        cursor.close()
 
         employees = [PersonEmployee(*row) for row in rows]
         return employees
@@ -18,33 +19,36 @@ class PersonEmployeeRepository:
         query = 'SELECT  * FROM \"pharma\".employees WHERE employee_id = %s;'
         cursor = self.connection.cursor()
         cursor.execute(query, (employee_id,))
-        row = cursor.fetchall()
+        row = cursor.fetchone()
+        cursor.close()
 
         return PersonEmployee(*row) if row else None
     
     def registry_employee(self, employee):
         query = '''
-            INSERT INTO \"pharma\".employees (employee_name, employee_cpf, employee_address, employee_telephone, employee_level_permission, employee_password, employee_admission_date);
+            INSERT INTO \"pharma\".employees (employee_name, employee_cpf, employee_address, employee_telephone, employee_level_permission, employee_password, employee_admission_date)
             VALUES(%s, %s, %s, %s, %s, %s, %s)
         '''
         cursor = self.connection.cursor()
         cursor.execute(query, (employee.employee_name, employee.employee_cpf, employee.employee_address, employee.employee_telephone, employee.employee_level_permission, employee.employee_password, employee.employee_admission_date))
         self.connection.commit()
+        cursor.close()
 
     def update_employee(self, employee):
         query = '''
             UPDATE \"pharma\".employees 
             SET employee_name = %s, employee_cpf = %s, employee_address = %s, employee_telephone = %s, employee_level_permission = %s, employee_password = %s, employee_admission_date = %s
-            WHERE employee_id = %s;
+            WHERE employee_id = %s
         '''
         cursor = self.connection.cursor()
-        cursor.execute(query, (employee.employee.employee_name, employee.employee_cpf, employee.employee_address, employee.employee_telephone, employee.employee_level_permission, employee.employee_password, employee.employee_admission_date, employee.employee_id))
+        cursor.execute(query, (employee.employee_name, employee.employee_cpf, employee.employee_address, employee.employee_telephone, employee.employee_level_permission, employee.employee_password, employee.employee_admission_date, employee.employee_id))
         self.connection.commit()
+        cursor.close()
 
     def delete_employee(self, employee_id):
-        query = 'DELETE FROM \"pharma\".employees WHERE employee_id = %s;'
+        query = 'DELETE FROM \"pharma\".employees WHERE employee_id = %s'
         cursor = self.connection.cursor()
         cursor.execute(query, (employee_id,))
         self.connection.commit()
-
+        cursor.close()
 

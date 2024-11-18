1dc5a4f0aa7881599497618a492a8c222f5d0a1a
Jean Marcos <j.marcoscor04@gmail.com>
2024-11-18 11:13:41 -0300

    Acrescentando repositorio produtos e alterando entidade produtos

--- src/model/entities/products.py
+++ src/model/entities/products.py
@@ -1,7 +1,8 @@
 class Product:
-    def __init__(self, product_id: int, product_name: str, product_price: float, category_id: int, product_stripe: str): ##Stripe = Tarja
+    def __init__(self, product_id: int, product_name: str, product_price: float, quantity_stock: int, category_id: int, product_stripe: str): ##Stripe = Tarja
         self.product_id = product_id
         self.product_name = product_name
         self.product_price = product_price
+        self.quantity_stock = quantity_stock
         self.category_id= category_id
         self.product_stripe = product_stripe
---
+++ src/model/repository/product_repository.py
@@ -0,0 +1,55 @@
+from src.model.entities.products import Product
+from src.model.connections.db import create_connection
+
+class ProductRepository:
+    def __init__(self):
+        self.connection = create_connection()
+    
+    def report_all_products(self):
+        query = 'SELECT * FROM \"pharma\".products'
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        cursor.close()
+
+        products = [Product(*row) for row in rows]
+        return products
+    
+    def find_product_by_id(self, product_id):
+        query = 'SELECT * FROM \"pharma\".products WHERE product_id = %s'
+        cursor = self.connection.cursor()
+        cursor.execute(query, (product_id,))
+        row = cursor.fetchone()
+        cursor.close()
+
+        return Product(*row) if row else None
+    
+    def create_product(self, product):
+        query = '''
+            INSERT INTO \"pharma\".products (product_name, product_price, quantity_stock, category_id, product_stripe)
+            VALUES (%s, %s, %s, %s, %s)   
+        '''
+        cursor = self.connection.cursor()
+        cursor.execute(query, (product.product_name, product.product_price, product.quantity_stock, product.category_id, product.product_stripe))
+        self.connection.commit()
+        cursor.close()
+
+    def update_product(self, product):
+        query = '''
+            UPDATE \"pharma\".products
+            SET product_name = %s, product_price = %s, quantity_stock = %s, category_id = %s, product_stripe = %s
+            WHERE product_id = %s
+        '''
+        cursor = self.connection.cursor()
+        cursor.execute(query, (product.product_name, product.product_price, product.quantity_stock, product.category_id, product.product_stripe, product.product_id))
+        self.connection.commit()
+        cursor.close()
+
+    def delete_product(self, product_id):
+        query = 'DELETE FROM \"pharma\".products WHERE product_id = %s'
+        cursor = self.connection.cursor()
+        cursor.execute(query, (product_id,))
+        self.connection.commit()
+        cursor.close()
+
+    

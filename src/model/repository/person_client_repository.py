1b9167397dc7e8cb14a57fef01b6b0c37f4e5529
Jean Marcos <j.marcoscor04@gmail.com>
2024-11-18 08:41:50 -0300

    Acrescenta repositório de clientes

---
+++ src/model/repository/person_client_repository.py
@@ -0,0 +1,48 @@
+from src.model.entities.person_client import PersonClient
+from src.model.connections.db import create_connection
+
+class PersonClientRepository:
+    def __init__(self):
+        self.connection = create_connection()
+
+    def report_all_clients(self):
+        query = 'SELECT * FROM \"pharma\".clients;'
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        clients = [PersonClient(*row) for row in rows]
+        return clients
+    
+    def find_client_by_id(self, client_id):
+        query = 'SELECT * FROM \"pharma\".clients WHERE client_id = %s;'
+        cursor = self.connection.cursor()
+        cursor.execute(query, (client_id,))
+        row = cursor.fetchall()
+
+        return PersonClient(*row) if row else None
+    
+    def registry_client(self, client):
+        query = '''
+            INSERT INTO \"pharma\".clients (client_name, client_cpf, client_address, client_telephone, client_registration_date)
+            VALUES(%s, %s, %s, %s, %s);
+        '''
+        cursor = self.connection.cursor()
+        cursor.execute(query, (client.client_name, client.client_cpf, client.client_address, client.client_telephone, client.client_registration_date))
+        self.connection.commit()
+    
+    def update_client(self, client):
+        query = '''
+            UPDATE \"pharma\".client
+            SET client_name = %s, client_cpf = %s, client_addess = %s, client_telephone = %s, client_registration_date = %s
+            WHERE client_id = %s;
+        '''
+        cursor = self.connection.cursor()
+        cursor.execute(query, (client.client_name, client.client_cpf, client.client_address, client.client_telephone, client.client_registration_date, client.client_id))
+        self.connection.commit()
+    
+    def delete_client(self, client_id):
+        query = 'DELETE FROM \"pharma\".clients WHERE client_id = %s;'
+        cursor = self.connection.cursor()
+        cursor.execute(query, (client_id,))
+        self.connection.commit()
--- src/model/repository/person_employee_repository.py
+++ src/model/repository/person_employee_repository.py
@@ -25,6 +25,7 @@ class PersonEmployeeRepository:
     def registry_employee(self, employee):
         query = '''
             INSERT INTO \"pharma\".employees (employee_name, employee_cpf, employee_address, employee_telephone, employee_level_permission, employee_password, employee_admission_date);
+            VALUES(%s, %s, %s, %s, %s, %s, %s)
         '''
         cursor = self.connection.cursor()
         cursor.execute(query, (employee.employee_name, employee.employee_cpf, employee.employee_address, employee.employee_telephone, employee.employee_level_permission, employee.employee_password, employee.employee_admission_date))
@@ -33,15 +34,15 @@ class PersonEmployeeRepository:
     def update_employee(self, employee):
         query = '''
             UPDATE \"pharma\".employees 
-            SET employee_name = %s, employee_cpf = %s, employee_address = %s, employee_telephone = %s, employee_level_permission = %s, employee_password = %s, employee_admission_date = %s;
-            WHERE employee_id = %s
+            SET employee_name = %s, employee_cpf = %s, employee_address = %s, employee_telephone = %s, employee_level_permission = %s, employee_password = %s, employee_admission_date = %s
+            WHERE employee_id = %s;
         '''
         cursor = self.connection.cursor()
         cursor.execute(query, (employee.employee.employee_name, employee.employee_cpf, employee.employee_address, employee.employee_telephone, employee.employee_level_permission, employee.employee_password, employee.employee_admission_date, employee.employee_id))
         self.connection.commit()
 
     def delete_employee(self, employee_id):
-        query = 'DELETE FROM \"pharma\".employees WHERE employee_id = %s'
+        query = 'DELETE FROM \"pharma\".employees WHERE employee_id = %s;'
         cursor = self.connection.cursor()
         cursor.execute(query, (employee_id,))
         self.connection.commit()
